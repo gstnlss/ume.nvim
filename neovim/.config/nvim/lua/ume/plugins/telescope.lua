9ce89e2306984409d4d62d2610a9350464fc74d1
Agustin Lessa <agustinlessa@pm.me>
2024-10-18 14:33:45 -0300

    fixes for macos and new telescope binding for hidden files

--- neovim/.config/nvim/lua/ume/plugins/telescope.lua
+++ neovim/.config/nvim/lua/ume/plugins/telescope.lua
@@ -1,34 +1,41 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      local utils = require('ume.utils')
-      local builtin = require('telescope.builtin')
+      local utils = require("ume.utils")
+      local builtin = require("telescope.builtin")
 
       local keymaps = {
-        { 'n', '<leader>ff', builtin.find_files },
-        { 'n', '<leader>fg', builtin.live_grep },
-        { 'n', '<leader>fb', builtin.buffers },
-        { 'n', '<leader>fh', builtin.help_tags },
-        { 'n', '<leader>fc', builtin.git_status }
+        { "n", "<leader>ff", builtin.find_files },
+        { "n", "<leader>fg", builtin.live_grep },
+        { "n", "<leader>fb", builtin.buffers },
+        { "n", "<leader>fd", builtin.help_tags },
+        { "n", "<leader>fc", builtin.git_status },
+        {
+          "n",
+          "<leader>fh",
+          function()
+            builtin.find_files({ hidden = true })
+          end,
+        },
       }
 
       utils.set_keymaps(keymaps)
-    end
+    end,
   },
   {
-    'nvim-telescope/telescope-ui-select.nvim',
+    "nvim-telescope/telescope-ui-select.nvim",
     config = function()
-      require('telescope').setup({
+      require("telescope").setup({
         extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown({})
-          }
-        }
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
       })
-      require('telescope').load_extension('ui-select')
-    end
-  }
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
