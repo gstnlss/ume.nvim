392bbb41713ba775c924a5e217dc24cf56cb030c
Agustin Lessa <agustinlessa@pm.me>
2024-10-19 15:36:21 -0300

    catppuccin-mocha theme for most configs

--- neovim/.config/nvim/lua/ume/config/lazy.lua
+++ neovim/.config/nvim/lua/ume/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -20,7 +20,6 @@ require("lazy").setup({
   spec = {
     { import = "ume.plugins" },
   },
-  install = { colorscheme = { "tokyonight" } },
   checker = { enabled = true },
   change_detection = {
     notify = false,
--- neovim/.config/nvim/lua/ume/plugins/tokyonight.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  opts = { style = "moon" },
-  config = function()
-    vim.cmd.colorscheme('tokyonight')
-  end
-}
