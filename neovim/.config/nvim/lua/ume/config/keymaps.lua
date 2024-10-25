ad49195da5c0193e3400b58a066187db662a9e93
Agustin Lessa <agustinlessa@pm.me>
2024-10-24 23:44:38 -0300

    added some buffer keybindings

--- neovim/.config/nvim/lua/ume/config/keymaps.lua
+++ neovim/.config/nvim/lua/ume/config/keymaps.lua
@@ -1,14 +1,22 @@
 local utils = require("ume.utils")
 
 local keymaps = {
+  { "n", "<leader>q", "<CMD>wqall<CR>" },
+  -- tab keymaps
   { "n", "<leader>tn", "<CMD>tabnew<CR>" },
   { "n", "<leader>tc", "<CMD>tabclose<CR>" },
   { "n", "<leader>th", "<CMD>tabprevious<CR>" },
   { "n", "<leader>tl", "<CMD>tabnext<CR>" },
+  -- buffer keymaps
+  { "n", "<leader>bd", "<CMD>bdelete<CR>" },
   {
     "n",
-    "<leader>q",
-    "<CMD>wqall<CR>",
+    "<leader>bc",
+    function()
+      vim.cmd([[%bd|e#]])
+      local no_name_buf = vim.fn.bufnr("$")
+      vim.api.nvim_buf_delete(no_name_buf, {})
+    end,
   },
 }
 
