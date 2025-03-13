f97ab987f31973523e6dd00e979317ec4b517cec
Agustin Lessa <agustinlessa@pm.me>
2025-03-12 21:44:27 -0300

    replace gen.nvim for a more mature ai plugin

--- config/nvim/lua/ume/plugins/ai.lua
+++ config/nvim/lua/ume/plugins/ai.lua
@@ -1,19 +1,77 @@
+-- Choose default model from a list of preferred models. Fall back to first
+-- available.
+local function preferred_model_picker(preferred)
+  return function(self)
+    if self == nil then
+      -- Debug:render() calls Adapter.schema.model.default() but doesn't pass
+      -- in `self`. Just return an empty string in this case.
+      return ""
+    end
+
+    local choices = self.schema.model.choices(self)
+
+    for _, best in ipairs(preferred) do
+      for _, choice in ipairs(choices) do
+        if choice:find(best, 1, true) then
+          return choice
+        end
+      end
+    end
+
+    return choices[1]
+  end
+end
+
 return {
-  "David-Kunz/gen.nvim",
-  config = function()
-    local gen = require("gen")
-    local utils = require("ume.utils")
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      local companion = require("codecompanion")
+      local utils = require("ume.utils")
 
-    gen.setup({
-      model = "qwen2.5-coder:14b",
-      show_prompt = true,
-      show_model = true,
-    })
+      companion.setup({
+        strategies = {
+          chat = { adapter = "ollama" },
+          inline = { adapter = "ollama_inline" },
+        },
+        adapters = {
+          ollama = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              schema = {
+                model = {
+                  default = preferred_model_picker({
+                    "qwen2.5-coder:14b",
+                    "qwen2.5-coder:7b",
+                    "qwen2.5-coder:3b",
+                    "qwen2.5-coder:1.5b",
+                    "qwen2.5-coder:0.5b",
+                  }),
+                },
+              },
+            })
+          end,
+          ollama_inline = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              schema = {
+                model = {
+                  default = "qwen2.5-coder:1.5b",
+                },
+              },
+            })
+          end,
+        },
+      })
 
-    utils.set_keymaps({
-      { { "n", "v" }, "<leader>ai", ":Gen<CR>" },
-      { "n", "<leader>ac", ":Gen Chat<CR>" },
-      { { "n", "v" }, "<leader>as", gen.select_model },
-    })
-  end,
+      utils.set_keymaps({
+        { { "n", "v" }, "<leader>ai", ":CodeCompanionActions<CR>" },
+        { { "n", "v" }, "<leader>ac", ":CodeCompanionChat<CR>" },
+        { { "n", "v" }, "<leader>at", ":CodeCompanionChat Toggle<CR>" },
+        { { "n", "v" }, "<leader>aq", ":CodeCompanion " },
+      })
+    end,
+  },
 }
--- config/nvim/lua/ume/plugins/lualine.lua
+++ config/nvim/lua/ume/plugins/lualine.lua
@@ -1,15 +1,18 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require('lualine').setup({
+    require("lualine").setup({
       options = {
-        theme = 'auto',
-        section_separators = '',
-        component_separators = '',
-        disabled_filetypes = { 'neo-tree' },
-        globalstatus = true
+        theme = "auto",
+        section_separators = "",
+        component_separators = "",
+        disabled_filetypes = { "neo-tree" },
+        globalstatus = true,
+      },
+      sections = {
+        lualine_c = { "filename", require("ume.config.lualine_codecompanion_component") },
       },
     })
-  end
+  end,
 }
